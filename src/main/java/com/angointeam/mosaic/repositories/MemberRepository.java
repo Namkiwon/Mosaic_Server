444b84d046b30b2a278c97210bef5f31208f5c06
daesoon <daesoon.choi@navercorp.com>
2018-08-15 08:52:51 +0900

    유저 캐싱 추가

--- src/main/java/com/angointeam/mosaic/repositories/MemberRepository.java
+++ src/main/java/com/angointeam/mosaic/repositories/MemberRepository.java
@@ -1,6 +1,8 @@
 package com.angointeam.mosaic.repositories;
 
 import com.angointeam.mosaic.domain.Member;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,8 +10,16 @@ import java.util.Optional;
 public interface MemberRepository  extends JpaRepository<Member,Long> {
 
     Optional<Member> findMemberByUuidAndAuthKey(String uuid, String authKey);
+
+    @Cacheable("byUuid")
     Optional<Member> findMemberByUuid(String uuid);
+
     Optional<Member> findMemberByNick(String nick);
+
     Optional<Member> findMemberByEmail(String email);
+
+    @Override
+    @CacheEvict("byUuid")
+    <S extends Member> S save(S var1);
 
 }
