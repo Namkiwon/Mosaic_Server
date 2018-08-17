79c5f0d8d3989e15dc784057fc6d21e719dffc8a
Namkiwon <originman@bluehack.net>
2018-08-17 17:04:04 +0900

    스크립 삭제시 스크랩 삭제

--- src/main/java/com/angointeam/mosaic/repositories/ScrapRepository.java
+++ src/main/java/com/angointeam/mosaic/repositories/ScrapRepository.java
@@ -15,10 +15,10 @@ public interface ScrapRepository extends JpaRepository<Scrap,Long> {
 
     List<Scrap> findAllByMemberUuid(String uuid);
 
-    @Query("SELECT s.script From Scrap s Where  s.memberUuid = ?1")
+    @Query("SELECT s.script From Scrap s Where  s.memberUuid = ?1 and s.script.valid = true")
     List<Script> findScriptListByMemberUuid(String uuid);
 
-    @Query("SELECT s.script.uuid From Scrap s Where  s.memberUuid = ?1")
+    @Query("SELECT s.script.uuid From Scrap s Where  s.memberUuid = ?1 and s.script.valid = true")
     List<String> findScriptUuidListByMemberUuid(String uuid);
 
     @Query("SELECT s From Scrap s Where  s.script.uuid = ?1 and s.memberUuid = ?2")
