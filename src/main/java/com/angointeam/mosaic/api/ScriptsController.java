45c38642c52c4e3590ac718006ac25fb0fca8a80
namkiwon <namkiwon@namkiwonui-MacBook-Pro.local>
2018-08-19 15:46:35 +0900

    add ModelAttribute 어노테이션

--- src/main/java/com/angointeam/mosaic/api/ScriptsController.java
+++ src/main/java/com/angointeam/mosaic/api/ScriptsController.java
@@ -64,7 +64,7 @@ public class ScriptsController {
             @ApiImplicitParam(name = "Authorization", value = "authorization : Bearer {token}", required = true
                     , dataType = "string", paramType = "header")
     })
-    public BaseResponse<List<Script>> getAll(@AuthenticationPrincipal @ApiIgnore final Member member,List<String> categories) {
+    public BaseResponse<List<Script>> getAll(@AuthenticationPrincipal @ApiIgnore final Member member,@ModelAttribute List<String> categories) {
 //        List<String> categories = new ArrayList<>();
         List<Script> scriptList = scriptsService.getAllScripts(member.getUuid(),categories);
         System.out.println(categories);
