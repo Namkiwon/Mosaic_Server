f1c85d41f325a0acc2172776bead33cf16b1adc7
daesoon <daesoon.choi@navercorp.com>
2018-08-25 03:22:18 +0900

    이메일 로그 작성

--- src/main/java/com/angointeam/mosaic/utils/email/EmailSender.java
+++ src/main/java/com/angointeam/mosaic/utils/email/EmailSender.java
@@ -28,6 +28,18 @@ public class EmailSender {
 
     public void send(String to, String uuid, String emailKey) throws Exception {
 
+        String html = "<h1>모자이크 가입 인증 메일</h1>"
+                + "<p>다음 링크를 누르시면 가입인증을 완료합니다. <a href='"+appHost+":"+appPort
+                + "/login/email/"+uuid+"/"+emailKey+"'>인증하기</a>";
+
+        String text = "다음 링크를 누르시면 가입 인증을 완료 합니다. "
+                + appHost + ":" + appPort
+                + "/login/email/"+uuid+"/"+emailKey;
+
+        System.out.println("send email with html : " + html);
+        System.out.println("send email with text : " + text);
+
+
         AmazonSimpleEmailService client =
                 AmazonSimpleEmailServiceClientBuilder.standard()
                         .withRegion(Regions.US_WEST_2).build();
@@ -37,13 +49,9 @@ public class EmailSender {
                 .withMessage(new Message()
                         .withBody(new Body()
                                 .withHtml(new Content()
-                                        .withCharset("UTF-8").withData("<h1>모자이크 가입 인증 메일</h1>"
-                                                + "<p>다음 링크를 누르시면 가입인증을 완료합니다. <a href='"+appHost+":"+appPort
-                                                + "/login/email/"+uuid+"/"+emailKey+"'>인증하기</a>"))
+                                        .withCharset("UTF-8").withData(html))
                                 .withText(new Content()
-                                        .withCharset("UTF-8").withData("다음 링크를 누르시면 가입 인증을 완료 합니다. "
-                                        + appHost + ":" + appPort
-                                        + "/login/email/"+uuid+"/"+emailKey)))
+                                        .withCharset("UTF-8").withData(text)))
                         .withSubject(new Content()
                                 .withCharset("UTF-8").withData(SUBJECT)))
                 .withSource(FROM);
